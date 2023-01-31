1efeb81195b974d4434717e59735c274867e19d4
Serhii Myshko <sergeiown@gmail.com>
2023-01-31 12:57:35 +0200

    investigate api responses

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 "use strict";
 
 async function fetchData() {
+  // const response = await fetch(
+  //   "http://www.omdbapi.com/?i=tt3896198&apikey=5b81342d"
+  // );
   const response = await fetch(
-    "http://www.omdbapi.com/?i=tt3896198&apikey=5b81342d"
+    "http://www.omdbapi.com/?s=world&page=1&apikey=5b81342d"
   );
+
   const data = await response.json();
   return data;
 }
 
 async function main() {
   const data = await fetchData();
+  showMovies(data);
   console.log(data);
 }
 
 main();
+
+function showMovies(response) {
+  const moviesResponse = response.Search;
+
+  try {
+    moviesResponse.forEach((movie) => {
+      document.querySelector(".films").innerHTML += `
+  <li>
+      <p>Title: ${movie.Title}</p>
+      <p>ID: ${movie.imdbID}</p><br>
+  </li>`;
+    });
+  } catch {}
+}
